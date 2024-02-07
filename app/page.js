4c6a81fd7f2b52308298f051ba420f63114fe6f4
s7xdd <90416778+s7xdd@users.noreply.github.com>
2024-02-08 01:46:30 +0530

    update

--- app/page.js
+++ app/page.js
@@ -1,16 +1,30 @@
 "use client";
+import { useRef } from "react";
 import { HiArrowCircleDown } from "react-icons/hi";
 import { IoMoonOutline } from "react-icons/io5";
 import { IoMdMoon } from "react-icons/io";
+import { SlSocialLinkedin } from "react-icons/sl";
+import { SlSocialGithub } from "react-icons/sl";
+import 'bootstrap/dist/css/bootstrap.css'
+import Aboutme from '@/components/Aboutme.jsx'
+
 
 
 
 export default function Home() {
+
+  const scrollToBottom = () => {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <div>
       <section className="min-h-screen">
-        <nav className="py-5 flex justify-between px-10">
-          <h1 className="text-white hover:text-red-400 cursor-none" style={{fontFamily: "SingleDay"}}>little did u know</h1>
+        <nav className="py-3 flex justify-between px-10">
+          <h1 className="text-white hover:text-red-400 cursor-none text-xl" style={{fontFamily: "SingleDay"}}>little did u know</h1>
           <ul className="flex items-center">
 
             <li className="px-4">
@@ -22,8 +36,9 @@ export default function Home() {
 
             <li>
               <a
-                className="text-white hover:text-gray-400"
-                href="/Resume">Resume</a>
+                className="text-black hover:text-gold p-2 px-3 text-xs rounded-xl bg-white"
+                style={{ textDecoration: "none" }}
+                href="/ResumeNew.pdf">Resume</a>
             </li>
           </ul>
         </nav>
@@ -32,20 +47,39 @@ export default function Home() {
           {/* <h1 className="text-2xl text-white">Hello world</h1> */}
         </div>
 
-        <div className="text-center flex col-span-1 justify-center py-44">
+        <div className="text-center flex col-span-1 justify-center py-36">
           <div>
             <div>
               <h1 className="text-white font-thin flex justify-center">Hello world! My name is</h1>
-              <h1 className="text-8xl text-white mt-2 font-bold">Mohammed Shamil</h1>
-              <h1 className="text-white hover:text-4xl transition-all duration-300 flex justify-center text-2xl mt-3">a passionate future software developer</h1>  
+              <h1 className="text-8xl text-teal-200 mt-2 font-bold">Mohammed Shamil</h1>
+              <h1 className="text-white hover:text-3xl transition-all duration-500 flex justify-center text-xl mt-3">a soon to be software developer</h1>  
+            </div>
+
+            <div>
+              <div className="flex col-span-2 justify-center space-x-4 mt-6 ">
+                  <a href="https://github.com/s7xdd">
+                      <SlSocialGithub 
+                        className="w-6 h-6 hover:size-7 transition-all duration-300" 
+                        style={{color: "goldenrod"}}
+                      />   
+                  </a>
+
+                  <a href="https://www.linkedin.com/in/kmshamil/">
+                      <SlSocialLinkedin 
+                        className="w-6 h-6 hover:size-7 transition-all duration-300"
+                        style={{color: "goldenrod"}}
+                      />   
+                  </a>
+                
+              </div>
             </div>
 
             <div className="flex justify-center mt-12">
                 <HiArrowCircleDown  
                   className="animate-bounce w-16 h-16 cursor-pointer" 
-                  style={{color: "white"}}
+                  style={{color: "white", textDecoration: "none"}}
                   onClick={() => {
-                    console.log("I was clicked");
+                    scrollToBottom();
                   }}
                   />
             </div>
@@ -63,6 +97,22 @@ export default function Home() {
               <h1 className="text-center text-white flex justify-center text-2xl ">I am currently a Computer Science student doing my final year</h1>
             </div>
           </section>
+        </div>
+
+        <div>
+            <div className="flex justify-center mt-10">
+              <Aboutme />
+            </div>
+
+            {/* <div className="mx-8 mt-16 bg-white rounded-3xl h-80 sm:mx-12">
+                <div className="text-center">
+                  <div>
+                      <h1 className="font-bold p-8 text-teal-600">ABOUT ME</h1>
+                      <p></p>
+                  </div>
+                </div>
+            </div> */}
+
         </div>
 
 
---
+++ components/Aboutme.jsx
@@ -0,0 +1,14 @@
+
+export default function Aboutme(){
+    return(
+        <div>
+            <div className="card text-bg-info mb-5" style={{maxwidth: "40rem"}}>
+                <div className="card-header text-center">About me</div>
+                <div className="card-body">
+                    <h5 className="card-title text-center">Info card title</h5>
+                    <p className="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+      </div>
+    )
+}
